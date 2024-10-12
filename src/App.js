20b05c17d63d75c56e7d9f323360f2d9d1009f96
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2024-10-12 22:41:30 +0200

    added Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-// import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import NotFound from "./pages/NotFound/NotFound";
+import SignInPage from "./pages/SignInPage/SignInPage";
 
 function App() {
   return (
-    <div className="App">
-      <WelcomePage />
+    <div>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/registration" element={<RegisterPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
