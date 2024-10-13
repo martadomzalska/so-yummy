3b1b1fe30868e2de221d8f2d494adbaaebc7a079
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2024-10-13 22:07:06 +0200

    signin page update + styles for desktop

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import { ReactComponent as PictureMobile } from "../../images/RegisterPage/picture-mobile.svg";
 import { ReactComponent as PictureTablet } from "../../images/RegisterPage/picture-tablet.svg";
+import { ReactComponent as PictureDesktop } from "../../images/RegisterPage/picture-desktop.svg";
 import { ReactComponent as EmailIconMobile } from "../../images/RegisterPage/emailIcon.svg";
 import { ReactComponent as EmailIconTablet } from "../../images/RegisterPage/emailInput-tablet.svg";
 import { ReactComponent as PasswordIconMobile } from "../../images/RegisterPage/passwordIcon.svg";
@@ -10,7 +11,14 @@ import css from "./SignIn.module.css";
 
 function SignInPage() {
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
-  const Picture = isMobile ? PictureMobile : PictureTablet;
+  const isTablet = useMediaQuery({
+    query: "(min-width: 768px) and (max-width: 1023px)",
+  });
+  const Picture = isMobile
+    ? PictureMobile
+    : isTablet
+    ? PictureTablet
+    : PictureDesktop;
   const EmailIcon = isMobile ? EmailIconMobile : EmailIconTablet;
   const PasswordIcon = isMobile ? PasswordIconMobile : PasswordIconTablet;
   return (
@@ -18,34 +26,36 @@ function SignInPage() {
       <div className={css.picture}>
         <Picture />
       </div>
-      <form className={css.form}>
-        <label className={css.label}>Sign In</label>
-        <div className={css.inputContainer}>
-          <EmailIcon className={css.iconInside} />
-          <input
-            className={css.input}
-            type="email"
-            name="email"
-            placeholder="Email"
-          ></input>
-        </div>
-        <div className={css.inputContainer}>
-          <PasswordIcon className={css.iconInside} />
-          <input
-            className={css.input}
-            type="password"
-            name="password"
-            placeholder="Password"
-            minLength={8}
-          ></input>
-        </div>
-        <button className={css.button} type="submit">
-          Sign in
-        </button>
-      </form>
-      <Link className={css.link} to="/registration">
-        Registration
-      </Link>
+      <div>
+        <form className={css.form}>
+          <label className={css.label}>Sign In</label>
+          <div className={css.inputContainer}>
+            <EmailIcon className={css.iconInside} />
+            <input
+              className={css.input}
+              type="email"
+              name="email"
+              placeholder="Email"
+            ></input>
+          </div>
+          <div className={css.inputContainer}>
+            <PasswordIcon className={css.iconInside} />
+            <input
+              className={css.input}
+              type="password"
+              name="password"
+              placeholder="Password"
+              minLength={8}
+            ></input>
+          </div>
+          <button className={css.button} type="submit">
+            Sign in
+          </button>
+        </form>
+        <Link className={css.link} to="/registration">
+          Registration
+        </Link>
+      </div>
     </div>
   );
 }
