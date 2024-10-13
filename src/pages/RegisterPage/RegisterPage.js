c766d0d61d508d38f37b48baec16d88c313b44d3
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2024-10-13 14:34:42 +0200

    updated RegisterPage
    -changed inputs and added icons
    -updated styles

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import { ReactComponent as PictureMobile } from "../../images/RegisterPage/picture-mobile.svg";
+import { ReactComponent as UserIcon } from "../../images/RegisterPage/userInput.svg";
+import { ReactComponent as EmailIcon } from "../../images/RegisterPage/emailIcon.svg";
+import { ReactComponent as PasswordIcon } from "../../images/RegisterPage/passwordIcon.svg";
 import css from "./RegisterPage.module.css";
 function RegisterPage() {
   return (
@@ -9,25 +12,35 @@ function RegisterPage() {
       </div>
       <form className={css.form}>
         <label className={css.label}>Registration</label>
-        <input
-          className={css.input}
-          type="text"
-          name="username"
-          placeholder="Name"
-        ></input>
-        <input
-          className={css.input}
-          type="email"
-          name="email"
-          placeholder="Email"
-        ></input>
-        <input
-          className={css.input}
-          type="password"
-          name="password"
-          placeholder="Password"
-          minLength={8}
-        ></input>
+        <div className={css.inputContainer}>
+          <UserIcon className={css.iconInside} />
+          <input
+            className={css.input}
+            type="text"
+            name="username"
+            placeholder="Name"
+          ></input>
+        </div>
+        <div className={css.inputContainer}>
+          <EmailIcon className={css.iconInside} />
+          <input
+            className={css.input}
+            type="email"
+            name="email"
+            placeholder="Email"
+          ></input>
+        </div>
+        <div className={css.inputContainer}>
+          <PasswordIcon className={css.iconInside} />
+          <input
+            className={css.input}
+            type="password"
+            name="password"
+            placeholder="Password"
+            minLength={8}
+          ></input>
+        </div>
+
         <button className={css.button} type="submit">
           Sign up
         </button>
