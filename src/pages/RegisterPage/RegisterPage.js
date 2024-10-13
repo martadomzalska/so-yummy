1a91186acfa26b7c5b615700aceff4eecd6f856c
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2024-10-13 21:24:04 +0200

    updated RegisterPage and styles

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,14 +1,25 @@
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import { ReactComponent as PictureMobile } from "../../images/RegisterPage/picture-mobile.svg";
-import { ReactComponent as UserIcon } from "../../images/RegisterPage/userInput.svg";
-import { ReactComponent as EmailIcon } from "../../images/RegisterPage/emailIcon.svg";
-import { ReactComponent as PasswordIcon } from "../../images/RegisterPage/passwordIcon.svg";
+import { ReactComponent as PictureTablet } from "../../images/RegisterPage/picture-tablet.svg";
+import { ReactComponent as UserIconMobile } from "../../images/RegisterPage/userInput.svg";
+import { ReactComponent as UserIconTablet } from "../../images/RegisterPage/userInput-tablet.svg";
+import { ReactComponent as EmailIconMobile } from "../../images/RegisterPage/emailIcon.svg";
+import { ReactComponent as EmailIconTablet } from "../../images/RegisterPage/emailInput-tablet.svg";
+import { ReactComponent as PasswordIconMobile } from "../../images/RegisterPage/passwordIcon.svg";
+import { ReactComponent as PasswordIconTablet } from "../../images/RegisterPage/passwordInput-tablet.svg";
 import css from "./RegisterPage.module.css";
 function RegisterPage() {
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+
+  const Picture = isMobile ? PictureMobile : PictureTablet;
+  const UserIcon = isMobile ? UserIconMobile : UserIconTablet;
+  const EmailIcon = isMobile ? EmailIconMobile : EmailIconTablet;
+  const PasswordIcon = isMobile ? PasswordIconMobile : PasswordIconTablet;
   return (
     <div className={css.container}>
       <div className={css.picture}>
-        <PictureMobile />
+        <Picture />
       </div>
       <form className={css.form}>
         <label className={css.label}>Registration</label>
