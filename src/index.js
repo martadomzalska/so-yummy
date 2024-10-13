beeaf9fe72f0ad470a8910077999549c86c9ed11
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2024-10-13 13:02:48 +0200

    added basename to router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/so-yummy-cookbook/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
